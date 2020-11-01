16eed59a8b3a3b31bcd25a31cb4af5214c2f51e9
Marko Sulaja <48557209+BataKole@users.noreply.github.com>
2020-11-01 10:44:22 +0100

    UnionFind Class added.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
 class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        PQueue<Integer> kju = new PQueue<>();
 
-        kju.add(1);
-        kju.add(1);
-        kju.add(2);
-        kju.add(2);
-        kju.add(3);
-        kju.add(0);
-        kju.add(-1);
-
-        while(!kju.isEmpty())
-            System.out.println(kju.poll());
-
+        
     }
+
 }
